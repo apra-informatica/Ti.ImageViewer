e988a1805ad8f8f9e2348b3bea93761ff27550dd
Mauro Piccotti <m.piccotti@apra.it>
2015-07-01 11:58:55 +0200

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Ti.ImageViewer
 Image viewer widget for Appcelerator Titanium
+
+With this image viewer it's possible to show a single - zoomable, scrollable and pinchable - image on iOS and Android.
+
+It uses module org.iotashan.TiTouchImageView (on Android), and installation of this module is necessary if you mean to use the widget on Android.
+
+## Installation
+
+Add in your *config.json*, under `dependencies`:
+
+```
+"dependencies": {
+    "it.apra.tiimageviewer": "*"
+}
+```
+
+## Usage
+```javascript
+widget = Widget.createWidget('it.apra.tiimageviewer', {
+	'image' : filepath,
+	'title' : filename,
+	'subtitle' : des
+});
+```
+
+**Args**
+* **image**: image filepath
+* **title**: optional image title (showed in lower panel)
+* **subtitle**: optional image subtitle (showed in lower panel)
+* **lowerInfoHided**: if true hides lower info panel
+* **lowerGradientHided**: if true hides lower info panel gradient
+* **backgroundColor**: backgroundColor (default is 'black')
+
+**Functions**
+* **removeEventListeners**: removes all listeners on controls
+* **reloadImage**: reloads the image
+* **showLowerInfo**: showes lower info
+* **hideLowerInfo**: hides lower info
+
+##Notes
+* Could be useful a compatibility on Android without the native module too (using a webview?)
+* The pinch-to-zoom and scroll on iOS are based on the widget https://github.com/CaffeinaLab/Ti.TiltImageView.
---
+++ controllers/widget.js
@@ -0,0 +1,182 @@
+var args = arguments[0] || {},
+	image = args.image,
+	title = args.title || "",
+	subtitle = args.subtitle || "",
+	lowerInfoHided = args.lowerInfoHided || false,
+	lowerGradientHided = args.lowerGradientHided || false,
+	backgroundColor = args.backgroundColor || 'black';
+
+var imageView, imageLoad, orientationChange, removeEventListeners, showLowerInfo;
+
+lastValidOrientation = null;
+
+showLowerInfo = function(){
+	if (!lowerInfoHided){
+		$.title.visible = true;
+		$.description.visible = true;
+	}
+};
+hideLowerInfo = function(){
+	$.title.visible = false;
+	$.description.visible = false;
+};
+
+if (OS_IOS) {
+	(function(){
+		var getMaxOffset, IMGS, OUTS, SS, LAYOUT, MINZOOMSCALE;
+		
+		imageView = Ti.UI.createImageView({
+			'image' : image,
+			'height': Ti.UI.SIZE,
+			'width': Ti.UI.SIZE,
+			'touchEnabled': false
+		});
+		$.imageViewContainer.add(imageView);
+		
+		getMaxOffset = function() {
+			return (IMGS.width*$.imageViewContainer.zoomScale)-OUTS.width;
+		};
+		
+		imageLoad = function(){
+			Ti.API.debug('Loading imagetView ' + title);
+			
+			var size, rect, OR, IR, zoomScaleToFit;
+			
+			size = imageView.size;
+			rect = $.index.size;
+			IMGS = { width: size.width, height: size.height };
+			OUTS = { width: rect.width, height: rect.height };
+			OR = OUTS.width/OUTS.height;
+			IR = IMGS.width/IMGS.height;
+	
+			// Recalculate if the min-height is not sufficient
+			if (IMGS.height<OUTS.height) {
+				imageView.height = OUTS.height;
+				IMGS = {
+					width: OUTS.height*(IMGS.width/IMGS.height),
+					height: OUTS.height
+				};
+			}
+			
+			// Scroll horizontal or vertically ?
+			MINZOOMSCALE = 1;
+			if (IR>OR) {
+				LAYOUT = 'H';
+				if (OUTS.height > 0 && IMGS.height > 0){
+					MINZOOMSCALE = OUTS.height/IMGS.height;
+				}
+				SS = OUTS.width-40;
+				$.scrollBar.applyProperties({
+					bottom: 90, left: 20, right: 20, top: null,
+					height: 1, width: Ti.UI.FILL
+				});
+				$.scrollBarInset.width = 60;
+			} else {
+				LAYOUT = 'V';
+				if (OUTS.width > 0 && IMGS.width > 0){
+					MINZOOMSCALE = OUTS.width/IMGS.width;
+				}
+				SS = OUTS.height-20;
+				$.scrollBar.applyProperties({
+					right: 20, left: null, top: 10, bottom: 10,
+					height: Ti.UI.FILL, width: 1
+				});
+				$.scrollBarInset.height = 60;
+			}
+	
+			// scalesToFit
+			if (IMGS.width > OUTS.width || IMGS.height > OUTS.height) {
+				zoomScaleToFit = Math.min(OUTS.height / IMGS.height, OUTS.width / IMGS.width);
+				if (zoomScaleToFit > 0 && zoomScaleToFit < 1){
+					MINZOOMSCALE = zoomScaleToFit;
+				}
+			}
+			MINZOOMSCALE *= 0.95;
+			
+			$.imageViewContainer.minZoomScale = MINZOOMSCALE;
+			$.imageViewContainer.zoomScale = MINZOOMSCALE;
+			$.imageViewContainer.maxZoomScale = 5;
+		};
+		
+		$.imageViewContainer.addEventListener('scroll', function(){
+			if (LAYOUT==='H') {
+				$.scrollBarInset.animate({
+					left: (SS-60)*($.imageViewContainer.contentOffset.x/getMaxOffset())
+				});
+			} else {
+				$.scrollBarInset.animate({
+					top: (SS-60)*($.imageViewContainer.contentOffset.y/getMaxOffset())
+				});
+			}
+		});
+		
+		imageView.addEventListener('load', imageLoad);
+		$.index.addEventListener('singletap', function(e) {
+			imageLoad();
+		});
+	}());
+} else if (OS_ANDROID){
+	(function(){
+		imageView = require('org.iotashan.TiTouchImageView').createView({
+			'image' : image,
+			'width': Ti.UI.FILL,
+			'height' : Ti.UI.FILL
+		});
+		$.imageViewContainer.add(imageView);
+		
+		imageLoad = function(){
+			imageView.resetZoom();
+		};
+	}());
+}
+
+(function(){
+	var lastValidOrientation;
+	
+	orientationChange = function(e){
+		if (e){
+			Ti.API.debug('Orientation: ' + e.orientation);
+			switch(e.orientation){
+				case Ti.UI.PORTRAIT:
+				case Ti.UI.UPSIDE_PORTRAIT:
+				case Ti.UI.LANDSCAPE_LEFT:
+				case Ti.UI.LANDSCAPE_RIGHT:
+					if (e.orientation === lastValidOrientation){
+						return;
+					}
+					lastValidOrientation = e.orientation;
+					break;
+				default:
+					return;
+			}
+		}
+		
+		imageLoad();
+	};
+	Ti.Gesture.addEventListener('orientationchange', orientationChange);
+}());
+
+removeEventListeners = function(){
+	Ti.API.debug('tiImageViewer: removingEventListeners ' + title);
+	
+	if (orientationChange) { Ti.Gesture.removeEventListener('orientationchange', orientationChange); }
+	if (imageView && imageLoad) { imageView.removeEventListener('load', imageLoad); }
+};
+
+
+$.title.text = title;
+$.description.text = subtitle;
+$.index.backgroundColor = backgroundColor;
+
+if (lowerInfoHided){
+	hideLowerInfo();
+} else {
+	showLowerInfo();
+}
+
+$.gradientImage.visible = !lowerGradientHided;
+
+exports.reloadImage = imageLoad;
+exports.removeEventListeners = removeEventListeners;
+exports.showLowerInfo = showLowerInfo;
+exports.hideLowerInfo = hideLowerInfo;
